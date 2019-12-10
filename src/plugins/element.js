dfa830a25151db0633b1c6c4946be0e62e40f81d
lihao <447730813@qq.com>
2019-12-10 16:34:34 +0800

    登录完成

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,12 +1,5 @@
 import Vue from 'vue'
-// eslint-disable-next-line import/no-duplicates
-import { Button } from 'element-ui'
-// eslint-disable-next-line import/no-duplicates
-import { Form, FormItem } from 'element-ui'
-// eslint-disable-next-line import/no-duplicates
-import { Input } from 'element-ui'
-// eslint-disable-next-line import/no-duplicates
-import { Message } from 'element-ui'
+import { Message, Form, FormItem, Input, Button } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(FormItem)
