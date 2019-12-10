6b3127beac8e9d0a3e894ae8f96645975107fd49
lihao <447730813@qq.com>
2019-12-10 15:16:45 +0800

    表单数据绑定

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,14 +1,89 @@
 <template>
   <div class="login_container">
-    登录组件
+    <div class="login_box">
+      <!--      头像区域-->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="">
+      </div>
+      <!--      登录表单区域-->
+      <el-form :model="loginForm" class="login_form">
+        <!--        用户名-->
+        <el-form-item>
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <!--        密码-->
+        <el-form-item>
+          <el-input type="password" v-model="loginForm.password" prefix-icon="iconfont icon-3702mima"></el-input>
+        </el-form-item>
+        <!--        按钮-->
+        <el-form-item class="bths">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 <script>
-export default {}
+export default {
+  data: function () {
+    return {
+      loginForm: {
+        // 这是登录表单的数据绑定对象
+        username: '',
+        password: ''
+      }
+    }
+  }
+}
 </script>
 <style lang="less" scoped>
-.login_container{
-  background-color: #2b4b6b;
-  height: 100%;
-}
+  .login_container {
+    background-color: #2b4b6b;
+    height: 100%;
+  }
+
+  .login_box {
+    width: 450px;
+    height: 300px;
+    background-color: #ffffff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eeeeee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #dddddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #ffffff;
+
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eeeeee;
+    }
+  }
+
+  .login_form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+  }
+
+  .bths {
+    display: flex;
+    justify-content: flex-end;
+  }
 </style>
